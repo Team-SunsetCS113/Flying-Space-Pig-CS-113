c7dd8806238472de06a0724c5ca809bd026d2782
peterhn <peterhn2@uci.edu>
2015-10-26 20:50:05 -0700

    added project references

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BackgroundController : MonoBehaviour
+    {
+        void FixedUpdate()
+        {
+            Vector2 movement = new Vector2(10, 10);
+            GetComponent<Rigidbody2D>().velocity = movement;
+
+            GetComponent<Rigidbody2D>().position = new Vector2
+        (
+                Mathf.Clamp(GetComponent<Rigidbody2D>().position.x, 0, 0),
+                Mathf.Clamp(GetComponent<Rigidbody2D>().position.y, 0, 0)
+        );
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,8 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = 0;// Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical") * 5;
 
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		GetComponent<Rigidbody2D>().velocity = movement*speed;
